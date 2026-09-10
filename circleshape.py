9774b679770a594f53589cb72d882962e7a965ad
nhojbg <xp0u405d@gmail.com>
2026-09-10 16:18:22 +0100

    B: Completion to CH3 L2

---
+++ circleshape.py
@@ -0,0 +1,26 @@
+import pygame
+
+
+# Base class for game objects
+class CircleShape(pygame.sprite.Sprite):
+    # Still not clear on the purpose of this container
+    containers: tuple[pygame.sprite.Group, ...]
+
+    def __init__(self, x: float, y: float, radius: float) -> None:
+        # we will be using this later
+        if hasattr(self, "containers"):
+            super().__init__(*self.containers) # The * 'unpacks' the tuple container rather than naming the individual objects within
+        else:
+            super().__init__()
+
+        self.position: pygame.Vector2 = pygame.Vector2(x, y)
+        self.velocity = pygame.Vector2(0, 0)
+        self.radius = radius
+
+    def draw(self, screen: pygame.Surface) -> None:
+        # must override
+        pass
+
+    def update(self, dt: float) -> None:
+        # must override
+        pass
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     print(f"Hello from asteroids!\nStarting Asteroids with pygame version: {pygame.version.ver}")
@@ -10,13 +11,22 @@ def main():
     
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
+    game_clock = pygame.time.Clock()
+    
+    dt = 0.0
+    
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+    
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = game_clock.tick(60) / 1000
+        #print(dt)
 
 
 if __name__ == "__main__":
